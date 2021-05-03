8725d3feda62e26ab5ec1b953329f55324a31a87
Ramiro Peidro <rpeidro@fi.uba.ar>
2021-05-03 17:01:33 -0300

    Primera maquetacion finalizada

---
+++ main.js
@@ -0,0 +1,55 @@
+ScrollReveal().reveal('.img-fluid', { 
+    duration: 1800,
+    distance: '50px',
+    origin: 'bottom',
+    reset: true
+ });
+
+ ScrollReveal().reveal('.contacto', { 
+    duration: 1500,
+    distance: '50px',
+    reset: true
+ });
+
+ ScrollReveal().reveal('.features', { 
+    duration: 2500,
+    distance: '50px',
+    reset: true
+ });
+
+ ScrollReveal().reveal('.page-header', { 
+    duration: 1500,
+    distance: '50px',
+    reset: true
+ });
+
+ ScrollReveal().reveal('.page-header', { 
+    duration: 1500,
+    distance: '50px',
+    reset: true
+ });
+
+ ScrollReveal().reveal('.burbujas', { 
+    duration: 2500,
+    distance: '50px',
+    reset: true
+ });
+
+ ScrollReveal().reveal('.works', { 
+    duration: 1500,
+    distance: '70px',
+    reset: true
+ });
+
+ ScrollReveal().reveal('.work-text', { 
+    duration: 1500,
+    distance: '70px',
+    reset: true
+ });
+
+
+ ScrollReveal().reveal('.social-media', { 
+    duration: 1500,
+    distance: '600px',
+    reset: true
+ });
